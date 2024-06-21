41c3d95aae5c8c1f97a928d2fdaa542c30163312
ahb0226@gmail.com <ahb0226@gmail.com>
2024-06-22 01:54:35 +0900

    tts 음성 불러오기 완료

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -16,6 +16,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
     databaseURL: 'https://ai-connectcar-default-rtdb.asia-southeast1.firebasedatabase.app/',
   ).reference();
   String _errorMessage = '';
+  List<bool> _selectedCarType = [true, false]; // Initial selection: Regular
 
   void _signUp() async {
     try {
@@ -26,10 +27,66 @@ class _SignUpScreenState extends State<SignUpScreen> {
       User? user = userCredential.user;
 
       if (user != null) {
-        await _database.child('users').child(user.uid).set({
-          'email': _emailController.text,
-          'carNumber': _carNumberController.text,
-        });
+        String carType = _selectedCarType[0] ? 'general' : 'emergency';
+        String vehicleNumber = _carNumberController.text;
+
+        if (carType == 'general') {
+          await _database.child(carType).child(vehicleNumber).set({
+            'email': _emailController.text,
+            'location': {
+              'lat': 0,
+              'long': 0,
+            },
+            'trigger': '',
+            'problem': {
+              'rxState': '',
+              'txState': '',
+              'myState': '',
+              'txText': '',
+              'rxText': '',
+              'myText': '',
+            },
+            'Service': {
+              'gasStation': {
+                'name': '',
+                'location': {
+                  'lat': 0,
+                  'long': 0,
+                },
+              },
+              'chargeStation': {
+                'name': '',
+                'location': {
+                  'lat': 0,
+                  'long': 0,
+                },
+              },
+            },
+            'report': {
+              '112': 0,
+              '119': 0,
+              '0800482000': 0,
+            },
+          });
+        } else {
+          await _database.child(carType).child(vehicleNumber).set({
+            'email': _emailController.text,
+            'location': {
+              'lat': 0,
+              'long': 0,
+            },
+            'trigger': '',
+            'problem': {
+              'egState': '',
+              'egText': '',
+            },
+            'intersectionGPS': {
+              'lat': 0,
+              'long': 0,
+            },
+          });
+        }
+
         _showSuccessDialog(context);
       }
     } on FirebaseAuthException catch (e) {
@@ -85,6 +142,28 @@ class _SignUpScreenState extends State<SignUpScreen> {
             TextField(
               controller: _carNumberController,
               decoration: InputDecoration(labelText: 'Car Number'),
+            ),
+            SizedBox(height: 20),
+            Text('Select Car Type:'),
+            ToggleButtons(
+              isSelected: _selectedCarType,
+              onPressed: (int index) {
+                setState(() {
+                  for (int i = 0; i < _selectedCarType.length; i++) {
+                    _selectedCarType[i] = i == index;
+                  }
+                });
+              },
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('Regular'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text('Emergency'),
+                ),
+              ],
             ),
             SizedBox(height: 20),
             if (_errorMessage.isNotEmpty)
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,10 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final DatabaseReference _database = FirebaseDatabase.instance.reference();
+  final DatabaseReference _database = FirebaseDatabase(
+    databaseURL: 'https://ai-connectcar-default-rtdb.asia-southeast1.firebasedatabase.app/',
+  ).reference();
+  final FlutterTts _flutterTts = FlutterTts();
+  User? _user;
+  String? _vehicleNumber;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = _auth.currentUser;
+    if (_user != null) {
+      _initializeTts();
+      _getVehicleNumberAndListenForTextUpdates();
+    } else {
+      print("User is not logged in");
+    }
+  }
+
+  void _initializeTts() {
+    _flutterTts.setStartHandler(() {
+      print("TTS playing");
+    });
+    _flutterTts.setCompletionHandler(() {
+      print("TTS complete");
+    });
+    _flutterTts.setErrorHandler((msg) {
+      print("TTS error: $msg");
+    });
+  }
+
+  void _getVehicleNumberAndListenForTextUpdates() async {
+    print("Getting vehicle number...");
+    DatabaseEvent generalEvent = await _database.child('general').orderByChild('email').equalTo(_user!.email).once();
+    DatabaseEvent emergencyEvent = await _database.child('emergency').orderByChild('email').equalTo(_user!.email).once();
+
+    bool isGeneralUser = generalEvent.snapshot.value != null;
+    bool isEmergencyUser = emergencyEvent.snapshot.value != null;
+
+    if (isGeneralUser) {
+      print("User type: general");
+      Map<dynamic, dynamic> generalData = generalEvent.snapshot.value as Map<dynamic, dynamic>;
+      _vehicleNumber = generalData.keys.first;
+      _listenForTextUpdates('general', _vehicleNumber!);
+    } else if (isEmergencyUser) {
+      print("User type: emergency");
+      Map<dynamic, dynamic> emergencyData = emergencyEvent.snapshot.value as Map<dynamic, dynamic>;
+      _vehicleNumber = emergencyData.keys.first;
+      _listenForTextUpdates('emergency', _vehicleNumber!);
+    } else {
+      print("User type: not found in database");
+    }
+  }
+
+  void _listenForTextUpdates(String userType, String vehicleNumber) {
+    print("Listening for text updates...");
+
+    _database.child(userType).child(vehicleNumber).child('problem').onValue.listen((event) {
+      DataSnapshot dataSnapshot = event.snapshot;
+      if (dataSnapshot.value != null) {
+        Map<dynamic, dynamic> values = dataSnapshot.value as Map<dynamic, dynamic>;
+        if (userType == 'general') {
+          _readTextIfNotEmpty(values['myText'], 'myText');
+          _readTextIfNotEmpty(values['rxText'], 'rxText');
+          _readTextIfNotEmpty(values['txText'], 'txText');
+        } else {
+          _readTextIfNotEmpty(values['egText'], 'egText');
+        }
+      } else {
+        print("No data in snapshot");
+      }
+    });
+  }
+
+  void _readTextIfNotEmpty(String? text, String fieldName) {
+    if (text != null && text.isNotEmpty) {
+      print("$fieldName text: $text");
+      _flutterTts.speak(text);
+    } else {
+      print("$fieldName is empty or null");
+    }
+  }
 
   void _logout(BuildContext context) async {
     await _auth.signOut();
