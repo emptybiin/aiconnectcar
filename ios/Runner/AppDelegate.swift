d31bfbdc64e2e6ef6aaa09ee0fafa894e86b4b1b
ahb0226@gmail.com <ahb0226@gmail.com>
2024-06-27 00:33:19 +0900

    googlemap 성공

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 import KakaoSDKCommon
 
 @UIApplicationMain
@@ -9,6 +10,7 @@ import KakaoSDKCommon
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     KakaoSDK.initSDK(appKey: "485169cb19d2eda65a5d36105f83a53b")
+    GMSServices.provideAPIKey("AIzaSyALSd3dqWMcVdun61oLS5xHUb2FL6kiFjQ")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
